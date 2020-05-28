e4b7035d2a4bc4984dc07f63c7b9b386f1eb4b24
frank-roesler <65922825+frank-roesler@users.noreply.github.com>
2020-05-28 14:16:26 +0100

    Added Flatten_curve.py

---
+++ Flatten_curve.py
@@ -0,0 +1,101 @@
+from celluloid import Camera
+import numpy as np
+import matplotlib.pyplot as pp
+
+
+N = 2000 # Number of people
+q = 0.0 # Percentage of vaccinated people
+D = 90 # Size of Domain
+
+T_recover = 30         # time to recover from illness
+T_immune = 50         # time after illness in which one is immune
+Spreading_distance = 1.5 # distance at which infected individuals infect others
+T = 600                # total running time
+    
+def dist(A,B):
+    # A = array of points
+    # B = single point
+    return np.sqrt((A[:,:1]-B[0])**2 + (A[:,1:2]-B[1])**2)
+
+
+lambda_f = 1
+
+def random_walk(A):
+    delta_x = np.random.uniform(-lambda_f,lambda_f,A.shape[0])[:,np.newaxis]
+    delta_y = np.random.uniform(-lambda_f,lambda_f,A.shape[0])[:,np.newaxis]
+    A[:,:1] = A[:,:1]+delta_x
+    A[:,1:2] = A[:,1:2]+delta_y
+    A[A[:,0]>D,:1] = -2*A[A[:,0]>D,:1]+3*D
+    A[A[:,1]>D,1:2] = 3*D-2*A[A[:,1]>D,1:2]
+    A[A[:,0]<0,:1] = -A[A[:,0]<0,:1]
+    A[A[:,1]<0,1:2] = -A[A[:,1]<0,1:2]
+
+    return A
+
+# Susceptibles:
+S = np.random.rand(N,2)*D
+
+I = np.zeros(N, dtype='int')
+I[0]=1
+# 0 = Susceptible
+# 1 = Infected
+# 2 = Immune
+
+Times = np.zeros(N)
+Times[0]=T_recover
+
+Times_immune = np.zeros(N)
+
+
+f, (P1,P2)=pp.subplots(ncols=2, figsize=(14,6))
+camera = Camera(f)
+
+L = []
+m=0
+n=1
+while len(I[I==1])!=0:
+# for r in range(T):
+    # print(n)
+    L.append(I[I==1].shape[0])
+    fill = [np.nan for k in range(T-len(L)+1)]
+    P1.plot(range(T+1),L+fill, color='r')
+    P1.axis([0, T, 0, N])
+    P2.plot(S[:,0],S[:,1],'.', c='b')
+    P2.plot(S[I==1,0],S[I==1,1],'.', c='r')
+    P2.plot(S[I==2,0],S[I==2,1],'.', c='y')
+    camera.snap()
+    # pp.plot(Vx,Vy,'.', c='y')
+    while n!=m:
+        n = I[I==1].shape[0]
+        infected = S[np.where(I==1)]   
+        J = np.copy(I)         
+        for i in range(len(infected)):
+            Infection_indices = np.where((dist(S,infected[i])<Spreading_distance)*(I!=2)[:,np.newaxis])[0]
+            I[Infection_indices] = 1 
+        m = I[I==1].shape[0]
+        Times[np.where(J!=I)] = T_recover
+    n=n-1
+
+    S=random_walk(S)
+    TT = np.copy(Times)>0
+    Times[Times>0] = Times[Times>0]-1
+    TT2 = Times>0
+    
+    J = np.copy(I)
+    I[TT!=TT2] = 2
+    
+    Times_immune[J!=I]=T_immune
+    TT = np.copy(Times_immune)>0
+    Times_immune[Times_immune>0] = Times_immune[Times_immune>0]-1
+    TT2 = Times_immune>0
+    I[TT!=TT2] = 0
+    
+
+anim = camera.animate() # repeat=False
+P2.legend(['Healthy','Infected', 'Immune'], loc='upper right', 
+          bbox_to_anchor=(0.8, 1.1), ncol=3)
+P1.legend(['Total number infected'], loc='upper right', 
+          bbox_to_anchor=(0.7, 1.1), ncol=3)
+
+# anim.save('flatten_curve3.html')
+
